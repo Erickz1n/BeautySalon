42038b3e108534c1a7b6d5e94053e455424d5e44
Erickz1n <erickmso0811@gmail.com>
2023-07-12 15:55:40 -0300

    add about us

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 // DOM - Document Object Model
 
+
+// ABRE E FECHA O MENU QUANDO CLICAR NO ICONE: HAMBURGUER E X
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll("nav .toggle")
 
 for(const element of toggle) {
     element.addEventListener('click', function() {
         nav.classList.toggle('show')
+    })
+}
+
+// QUANDO CLICAR EM UM ITEM DO MENU, ESCONDER O MENU
+
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
     })
 }
