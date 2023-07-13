a0166d57e445a7754fbda27e61f058de272ef254
Erickz1n <erickmso0811@gmail.com>
2023-07-13 20:05:34 -0300

    section testimonials and contact

--- main.js
+++ main.js
@@ -36,3 +36,31 @@ window.addEventListener('scroll', function () {
         header.classList.remove('scroll')
     }
 })
+
+// Testminoials swiper
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+/* ScrollReveal: mostrar elementos quando der scroll na pagina */
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '38px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .text, #home .image,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimnoials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `, {interval: 100})
