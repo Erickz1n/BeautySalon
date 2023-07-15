96749bf5c3e7a8d4bd149687f8d5057157c7f2f7
Erickz1n <erickmso0811@gmail.com>
2023-07-14 22:40:45 -0300

    add breakpoints

--- main.js
+++ main.js
@@ -22,10 +22,10 @@ for(const link of links) {
 }
 
 // mudar o header da página quando der scroll
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
 
 function changeHeaderWhenScroll() {
-    const header = document.querySelector("#header")
-    const navHeight = header.offsetHeight
 
 
     if(this.window.scrollY >= navHeight) {
@@ -76,9 +76,9 @@ scrollReveal.reveal(
 )
 
 // Botao voltar para o topo
+const backToTopButton = document.querySelector('.back-to-top')
 
 function backToTop() {
-    const backToTopButton = document.querySelector('.back-to-top')
 
     if (window.scrollY >= 560) {
         backToTopButton.classList.add('show')
@@ -86,10 +86,34 @@ function backToTop() {
         backToTopButton.classList.remove('show')
     }
 }
+// Menu ativo conforme a seção visível na página
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if(checkpointStart && checkpointEnd) {
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']'  )
+            .classList.add('active')
+        } else {
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']'  )
+            .classList.remove('active')
+        }
+    }
+}
 
 // When Scroll 
-
 window.addEventListener('scroll', function () {
     changeHeaderWhenScroll()
     backToTop()
+    activateMenuAtCurrentSection()
 })
